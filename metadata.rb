69cdd8cb6d9c5ea3a029236b7b25e99b1ce7dfd1
Sean M. Sullivan <sean@barriquesoft.com>
2014-11-11 11:32:57 -0800

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'networking-basic-cookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures networking-basic-cookbook'
+long_description 'Installs/Configures networking-basic-cookbook'
+version          '0.1.0'
